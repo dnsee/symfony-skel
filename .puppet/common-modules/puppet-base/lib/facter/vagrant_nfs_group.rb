8d89e4eba0edce34779445055ed46e7253c43e43
David Funaro <ing.davidino@gmail.com>
2013-10-09 00:35:56 +0200

    removed vagrant dnsee

--- .puppet/common-modules/puppet-base/lib/facter/vagrant_nfs_group.rb
+++
@@ -1,5 +0,0 @@
-Facter.add("vagrant_nfs_group") do
-  setcode do
-    Facter::Util::Resolution.exec("echo $(stat -c %g /vagrant)")
-  end
-end
--- .puppet/common-modules/symfony/manifests/init.pp
+++
@@ -1,9 +0,0 @@
-class symfony {
-    file { 'app_dev.php':
-        source => 'puppet:///modules/symfony/app_dev.php',
-        path => '/vagrant/web/app_dev.php',
-        ensure => present,
-        owner => $vagrant_nfs_user,
-        group => $vagrant_nfs_group
-    }
-}
