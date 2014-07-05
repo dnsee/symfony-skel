6c7c9c236b4533f431acde3dfeaf3073b5f0ecbf
Massimiliano Arione <garakkio@gmail.com>
2014-07-05 11:01:09 +0200

    update to latest 2.5 changes

--- web/app.php
+++ web/app.php
@@ -9,8 +9,9 @@ $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 // Change 'sf2' to a unique prefix in order to prevent cache key conflicts
 // with other applications also using APC.
 /*
-$loader = new ApcClassLoader('sf2', $loader);
-$loader->register(true);
+$apcLoader = new ApcClassLoader('sf2', $loader);
+$loader->unregister();
+$apcLoader->register(true);
 */
 
 require_once __DIR__.'/../app/AppKernel.php';
@@ -21,6 +22,9 @@ $debug = (bool) getenv('APP_DEBUG');
 $kernel = new AppKernel($env, $debug);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
+
+// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
+//Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
